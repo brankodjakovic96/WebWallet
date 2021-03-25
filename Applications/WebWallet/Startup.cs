869a0e5d2de6e79c6d49fbb315fba2d73de67186
Branko Djakovic <brankodjakovic08@gmail.com>
2021-03-25 02:34:59 +0100

    Dockerized the whole project.

--- Applications/WebWallet/Startup.cs
+++ Applications/WebWallet/Startup.cs
@@ -50,7 +50,7 @@ namespace Applications.WebWallet
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CoreEfCoreDbContext coreEfCoreDbContext)
         {
             if (env.IsDevelopment())
             {
@@ -62,7 +62,8 @@ namespace Applications.WebWallet
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            
+
+            coreEfCoreDbContext.Database.Migrate();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
