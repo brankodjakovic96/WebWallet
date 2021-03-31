6f8b0e7707c123097581f677e5cc65a85b162c61
Branko Djakovic <brankodjakovic08@gmail.com>
2021-03-31 15:34:01 +0200

    Added IsBlocked to Wallet.

---
+++ Core/Infrastructure/DataAccess/EfCoreDataAccess/Migrations/20210331133126_Added_IsBlocked_To_Wallet.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Core.Infrastructure.DataAccess.EfCoreDataAccess.Migrations
+{
+    public partial class Added_IsBlocked_To_Wallet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBlocked",
+                table: "Wallets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsBlocked",
+                table: "Wallets");
+        }
+    }
+}
